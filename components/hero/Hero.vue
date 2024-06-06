f98887db0d5ad2ee1fb81cca4d746860b1655bc8
ihorovich <118834609+ihorovich@users.noreply.github.com>
2024-06-07 00:45:21 +0300

    Add files via upload

---
+++ components/hero/Hero.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-blue p-10 lg:p-36">
+    <h1 class="text-white text-4xl mb-5">вебсайт вебсайт вебсайт</h1>
+    <h2 class="text-white text-2xl">вебсайт вебсайт вебсайт</h2>
+  </div>
+</template>
